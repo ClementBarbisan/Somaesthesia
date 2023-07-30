2bd8025e9ea81734438e5c47fee83e56e994b54d
Clément Barbisan <c.barbisan@free.fr>
2023-07-30 17:57:30 +0200

    change process to executable

--- Somaesthesia/Assets/Scripts/LaunchProcess.cs
+++ Somaesthesia/Assets/Scripts/LaunchProcess.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class LaunchProcess : MonoBehaviour
 {
@@ -12,21 +13,24 @@ public class LaunchProcess : MonoBehaviour
     [DllImport("user32.dll")] static extern bool SetForegroundWindow(IntPtr hWnd); 
     // Start is called before the first frame update
     private IntPtr windowUnity;
+    Process pr = new Process();
     void OnEnable()
     {
         windowUnity = (IntPtr) GetActiveWindow();
-        Process pr = new Process();
+       
         ProcessStartInfo prs = new ProcessStartInfo();
-        prs.FileName = @"python";
+        prs.FileName = "C:\\webcam_action_recognition\\webcam_action_recognition.exe";
+        prs.UseShellExecute = true;
         prs.Arguments =
-            "C:\\Git\\Somaesthesia\\mmaction2\\webcam_action_recognition.py C:\\tanet_imagenet-pretrained-r50_8xb8-dense-1x1x8-100e_kinetics400-rgb.py" +
-            " C:\\tanet_imagenet-pretrained-r50_8xb8-dense-1x1x8-100e_kinetics400-rgb_20220919-a34346bc.pth c:\\label_map_k400.txt --device cuda:0 --inference-fps 60 --image";
-        prs.CreateNoWindow = true;
+            "C:\\tanet_imagenet-pretrained-r50_8xb8-dense-1x1x8-100e_kinetics400-rgb.py" +
+            " C:\\tanet_imagenet-pretrained-r50_8xb8-dense-1x1x8-100e_kinetics400-rgb_20220919-a34346bc.pth" +
+            " C:\\label_map_k400.txt --device cuda:0 --inference-fps 60 --image True";
         pr.StartInfo = prs;
 
         ThreadStart ths = new ThreadStart(() => pr.Start());
         Thread th = new Thread(ths);
         th.Start();
+
     }
 
     private void Start()
@@ -37,6 +41,5 @@ public class LaunchProcess : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
